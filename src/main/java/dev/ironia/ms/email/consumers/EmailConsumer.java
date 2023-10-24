8abef852dc0027d65d701eedaa3e0bd85026c997
r41l1m4 <rai.lima17@gmail.com>
2023-10-24 09:36:33 -0300

    refactor EmailConsumer

--- src/main/java/dev/ironia/ms/email/consumers/EmailConsumer.java
+++ src/main/java/dev/ironia/ms/email/consumers/EmailConsumer.java
@@ -2,6 +2,7 @@ package dev.ironia.ms.email.consumers;
 
 import dev.ironia.ms.email.dtos.EmailRecordDto;
 import dev.ironia.ms.email.models.EmailModel;
+import dev.ironia.ms.email.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,11 +11,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailConsumer {
 
+    private EmailService emailService;
+
+    public EmailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto) {
         var emailModel = new EmailModel();
         BeanUtils.copyProperties(emailRecordDto, emailModel);
 
-
+        emailService.sendEmail(emailModel);
     }
 }
