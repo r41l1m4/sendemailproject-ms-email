70d81a4f651ae757890484c61f84ea841ed9adae
r41l1m4 <rai.lima17@gmail.com>
2023-10-24 09:08:34 -0300

    add email model/repository

---
+++ src/main/java/dev/ironia/ms/email/enums/StatusEmail.java
@@ -0,0 +1,6 @@
+package dev.ironia.ms.email.enums;
+
+public enum StatusEmail {
+    SENT,
+    ERROR;
+}
---
+++ src/main/java/dev/ironia/ms/email/repositories/EmailRepository.java
@@ -0,0 +1,9 @@
+package dev.ironia.ms.email.repositories;
+
+import dev.ironia.ms.email.models.EmailModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
+}
